02fd5a6eac5ba18a8c9442d473b6b4cf0043bbf7
ramji543 <dipanshusrivastava.735@gmail.com>
2025-09-05 22:35:16 +0530

    fix dotenv

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { Auth0Provider } from '@auth0/auth0-react'
 import TransactionProvider from './services/context/contextstate.jsx'
-import dotenv from dotenv
+import dotenv from "dotenv"
 dotenv.config();
 
 createRoot(document.getElementById('root')).render(
